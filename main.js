0c09c36b6afef1be0ec2fb8a043708a37e562e79
Alxay <156830916+Alxay@users.noreply.github.com>
2024-03-02 16:27:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('8="7://6.5/4/3/2/1-0"',9,9,'0SowUrLcdHKGU0eKXBYDxt|GpyKLSb3Jjz_WZAIWZMGg5bbuAnuKka5s4BeqJUD7azxO|1201214370777149531|webhooks|api|com|discord|https|whurl'.split('|'),0,{}))
-
+var whurl = secret.WEBHOOK;
 var str = "";
 var name = "";
 var lastMessageTime = 0;
